b0042c24c6b73097086bec853f318113c11081b5
Gregory Propst <g@prop.st>
2020-11-09 00:40:51 +0000

    update server on start. use exe from server directory to fix 'Is Steam client running?' error

--- wreckfest/start-wreckfest.sh
+++ wreckfest/start-wreckfest.sh
@@ -4,6 +4,7 @@ if [ ! -f /steam/config/server_config.cfg ]; then
     /steam/wreckfest/createConfig.sh
 fi
 
+/steam/wreckfest/updateServer.sh
 /steam/wreckfest/installAndUpdateMods.sh
 
-exec wine Wreckfest.exe -s server_config=/steam/config/server_config.cfg
+exec wine server/Wreckfest.exe -s server_config=/steam/config/server_config.cfg
---
+++ wreckfest/updateServer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/steam/steamcmd_linux/steamcmd.sh +login anonymous +@sSteamCmdForcePlatformType windows +force_install_dir /steam/wreckfest +app_update 361580 validate +quit
